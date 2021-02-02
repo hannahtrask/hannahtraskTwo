67451ae60fbd935444c4276bfbffd9169fb517fe
Hannah Catherine Trask <hctrask@gmail.com>
2021-02-02 08:32:16 -0700

    update

--- pages/work.js
+++ pages/work.js
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.scss';
 const Work = () => {
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.header}>work page</h1>
+			<h1 className={styles.header}>This is a work in progress :)</h1>
 			<div className={styles.clients}></div>
 			<div className={styles.projects}></div>
 		</div>
